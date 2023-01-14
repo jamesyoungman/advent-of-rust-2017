d6086c1eba7590094bf4cc96d3c89836dc0eb5a5
James Youngman <james@youngman.org>
2023-01-14 09:29:08 +0000

    Solved Day 1 part 2.

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -2,11 +2,8 @@ use std::str;
 
 fn part1(s: &str) -> u32 {
     let mut v: Vec<char> = s.trim_end().chars().collect();
-    match v.first() {
-        Some(ch) => {
-            v.push(*ch);
-        }
-        None => (),
+    if let Some(ch) = v.first() {
+        v.push(*ch);
     }
     v.windows(2)
         .filter_map(|w| {
@@ -34,8 +31,38 @@ fn test_day01_part1() {
     assert_eq!(part1("91212129\n"), 9);
 }
 
+fn part2(s: &str) -> u32 {
+    let v: Vec<char> = s.trim_end().chars().collect();
+    let len = v.len();
+    let halflen = len / 2;
+    (0..len)
+        .filter_map(|i| {
+            let next = (i + halflen) % len;
+            if v[i] == v[next] {
+                Some(
+                    v[i].to_digit(10)
+                        .expect("input should only contain decimal digits"),
+                )
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+#[test]
+fn test_day01_part2() {
+    assert_eq!(part2(""), 0);
+    assert_eq!(part2("\n"), 0);
+    assert_eq!(part2("1212\n"), 6);
+    assert_eq!(part2("1221\n"), 0);
+    assert_eq!(part2("123425\n"), 4);
+    assert_eq!(part2("123123\n"), 12);
+    assert_eq!(part2("12131415\n"), 4);
+}
+
 fn main() {
     let input = str::from_utf8(include_bytes!("input.txt")).expect("input should be value UTF-8");
-    // 1155 is too low.
     println!("Day 01 part 1: {}", part1(input));
+    println!("Day 01 part 2: {}", part2(input));
 }
