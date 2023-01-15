c2cf90ded9107334d0eb7f7836e845b3e78c79eb
James Youngman <james@youngman.org>
2023-01-15 09:01:11 +0000

    Solved day 4 part 2.

--- src/bin/day04/main.rs
+++ src/bin/day04/main.rs
@@ -18,16 +18,16 @@ fn test_contains_duplicate_word() {
     assert!(!contains_duplicate_word("aa bb cc dd aaa"));
 }
 
-fn count_valid_passphrases(s: &str) -> usize {
+fn count_part1_valid_passphrases(s: &str) -> usize {
     s.split_terminator('\n')
         .filter(|s| !contains_duplicate_word(s))
         .count()
 }
 
 #[test]
-fn test_count_valid_passphrases() {
+fn test_count_part1_valid_passphrases() {
     assert_eq!(
-        count_valid_passphrases(concat!(
+        count_part1_valid_passphrases(concat!(
             "aa bb cc dd ee\n",
             "aa bb cc dd aa\n",
             "aa bb cc dd aaa\n"
@@ -36,7 +36,28 @@ fn test_count_valid_passphrases() {
     );
 }
 
+fn contains_duplicate_anagram(s: &str) -> bool {
+    let mut words: HashSet<String> = HashSet::new();
+    for word in s.split_ascii_whitespace() {
+        let mut v: Vec<char> = word.chars().collect();
+        v.sort();
+        let canonical: String = v.iter().collect();
+
+        if !words.insert(canonical) {
+            return true;
+        }
+    }
+    false
+}
+
+fn count_part2_valid_passphrases(s: &str) -> usize {
+    s.split_terminator('\n')
+        .filter(|s| !contains_duplicate_anagram(s))
+        .count()
+}
+
 fn main() {
     let input = str::from_utf8(include_bytes!("input.txt")).expect("input should be value UTF-8");
-    println!("Day 04 part 1: {}", count_valid_passphrases(input));
+    println!("Day 04 part 1: {}", count_part1_valid_passphrases(input));
+    println!("Day 04 part 2: {}", count_part2_valid_passphrases(input));
 }
