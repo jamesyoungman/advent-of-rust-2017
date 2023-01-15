d924fea6c569738e0c9706de397c9c4e1fa4729a
James Youngman <james@youngman.org>
2023-01-15 09:18:05 +0000

    Solved day 5 part 1.

---
+++ src/bin/day05/main.rs
@@ -0,0 +1,67 @@
+use std::str;
+
+use lib::error::Fail;
+
+#[derive(Eq, PartialEq, Debug)]
+struct Code {
+    pc: isize,
+    instructions: Vec<i32>,
+}
+
+impl Code {
+    fn step(&mut self) -> bool {
+        match self.instructions.get_mut(self.pc as usize) {
+            Some(offset) => {
+                self.pc += *offset as isize;
+                *offset += 1;
+                true
+            }
+            None => false,
+        }
+    }
+}
+
+impl TryFrom<&str> for Code {
+    type Error = Fail;
+    fn try_from(s: &str) -> Result<Self, Fail> {
+        let instructions: Vec<i32> = s
+            .split_terminator('\n')
+            .map(|s| {
+                s.trim()
+                    .parse()
+                    .map_err(|e| Fail(format!("invalid digit '{s}: {e}")))
+            })
+            .collect::<Result<Vec<i32>, Fail>>()?;
+        Ok(Code {
+            pc: 0,
+            instructions,
+        })
+    }
+}
+
+#[test]
+fn test_parse() {
+    let input = "0\n3\n0\n  1\n  -3\n";
+    assert_eq!(
+        Code::try_from(input),
+        Ok(Code {
+            pc: 0,
+            instructions: vec![0, 3, 0, 1, -3]
+        })
+    )
+}
+
+fn part1(s: &str) -> usize {
+    let mut code = Code::try_from(s).expect("input should be valid");
+    for i in 0.. {
+        if !code.step() {
+            return i;
+        }
+    }
+    unreachable!()
+}
+
+fn main() {
+    let input = str::from_utf8(include_bytes!("input.txt")).expect("input should be valid UTF-8");
+    println!("Day 05 part 1: {}", part1(input));
+}
